71c4b7eac7b73451ceb6f3242cffa7ab5b8adf30
Gary Li <garyli0802@yahoo.com>
2022-09-28 12:03:24 -0700

    initial commit

--- InterfaceExercise/Program.cs
+++ InterfaceExercise/Program.cs
@@ -1,44 +1,99 @@
 ﻿using System;
-
+using InterfaceExercise;
+using static InterfaceExercise.Program;
 namespace InterfaceExercise
 {
     class Program
+    //TODO Be sure to follow BEST PRACTICES when creating classes and interfaces
+
+    //Create 2 Interfaces called IVehicle & ICompany
+
+
+    //Now, create objects of your 3 classes and give their members values;
+    //Creatively display and organize their values
     {
         static void Main(string[] args)
         {
-            //TODO Be sure to follow BEST PRACTICES when creating classes and interfaces
+            var car1 = new Car
+            {
+                HasFourDoors = false,
+                HasTrunk = true,
+                Brand = "Coca-Cola",
+                Slogan = "Always Coca-Cola",
+                Doors = 2,
+                Wheels = 4,
+                Lights = 7,
+                Mirrors = 3,
+                
+            };
+            var truck1 = new Truck
+            {
+                Brand = "Pepsi",
+                Slogan = "That's what I like",
+                Doors = 4,
+                Wheels = 4,
+                Lights = 8,
+                Mirrors = 3,
+                HasTowHook = true,
+                HasTruckBed = true,
+            };
+            var suv1 = new SUV
+            {
+                Brand = "Dr.Pepper",
+                Slogan = "King Of Beverages",
+                Doors = 4,
+                Wheels = 4,
+                Lights = 6,
+                Mirrors = 2,
+                HasCargoHold = true,
+                HasRoofRack = true,
+            };
 
-            //Create 2 Interfaces called IVehicle & ICompany
+            Console.WriteLine($"I put a logo on my car and its {car1.Brand} branded, the slogan of the brand is {car1.Slogan}.\nThe car has only 2 doors, {car1.HasFourDoors} so the car is a coupe so it has only {car1.Doors} doors.\n The rest of the car is basic, it has {car1.Wheels} wheels," +
+                $" {car1.Lights} lights, {car1.Mirrors} mirrors, and it has a trunk, {car1.HasTrunk}.");
+            Console.WriteLine("**************************************************************");
+            Console.WriteLine($"I put a logo on my truck and its {truck1.Brand} branded, the slogan of the brand is {truck1.Brand}.\nThe truck is a basic truck so it has {truck1.Doors} doors, {truck1.Wheels} wheels, {truck1.Lights} lights, {truck1.Mirrors} mirrors, and has a trunkbed, {truck1.HasTruckBed}.\n" +
+                $"The back of my truck also has a tow hook, {truck1.HasTowHook}.");
+            Console.WriteLine("**************************************************************");
+            Console.WriteLine($"I put a logo on my suv and its {suv1.Brand} branded, the slogan of the brand is {suv1.Slogan}.\nThe suv is a little damaged but works just fine. It has {suv1.Doors} doors, {suv1.Wheels} wheels, {suv1.Lights} lights.\n"
+              + $"The only damage is that it only has {suv1.Mirrors} total mirrors. My right side mirror is hinged off, but I will be fixing it soon.");
 
-            //Create 3 classes called Car , Truck , & SUV
 
-            //In your IVehicle
-            
-                /* Create 4 members that Car, Truck, & SUV all have in common.
-                 * Example: All vehicles have a number of wheels... for now..
-                 */
-            
 
-            //In ICompany
-            
-                /*Create 2 members that are specific to each every company
-                 * regardless of vehicle type.
-                 *
-                 *
-                 * Example: public string Logo { get; set; }
-                 */
+        }
 
-            //In each of your car, truck, and suv classes
+        //Create 3 classes called Car , Truck , & SUV
 
-                /*Create 2 members that are specific to each class
-                 * Example: truck has a bed size while car has a trunk while suv has a cargo hold size
-                 *
-                 * Then, Set each class to inherit from both IVehicle and ICompany and implement their members.
-                 * 
-                 */
+        //In your IVehicle
 
-            //Now, create objects of your 3 classes and give their members values;
-            //Creatively display and organize their values
+        /* Create 4 members that Car, Truck, & SUV all have in common.
+         * Example: All vehicles have a number of wheels... for now..
+         */
+        public interface IVehicle
+        {
+            public int Wheels { get; set; }
+            public int Doors { get; set; }
+            public int Lights { get; set; }
+            public int Mirrors { get; set; }
+        }
+
+        //In ICompany
+
+        /*Create 2 members that are specific to each every company
+         * regardless of vehicle type.
+         *
+         *
+         * Example: public string Logo { get; set; }
+         */
+        public interface ICompany
+        {
+            public string Slogan { get; set; }  
+            public string Brand { get; set; }   
+        }
+                      
+         
+        
+   
         }
     }
-}
+
---
+++ InterfaceExercise/Truck.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using InterfaceExercise;
+using static InterfaceExercise.Program;
+//In each of your car, truck, and suv classes
+
+/*Create 2 members that are specific to each class
+ * Example: truck has a bed size while car has a trunk while suv has a cargo hold size
+ *
+ * Then, Set each class to inherit from both IVehicle and ICompany and implement their members.
+ * 
+ */
+namespace InterfaceExercise 
+{
+    public class Truck: IVehicle, ICompany
+    {
+        public int Wheels { get; set; }
+        public int Doors { get; set; }
+        public int Lights { get; set; }
+        public int Mirrors { get; set; }
+       
+        public string Slogan { get; set; }
+        public string Brand { get; set; }
+        public bool HasTruckBed { get; set; }
+        public bool HasTowHook { get; set; }    
+    }
+}
